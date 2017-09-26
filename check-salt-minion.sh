8c9a19cae84cd31b75883dde330d77ec7050df93
Mohammad Ali Alfarra <mohammadali@sam-media.com>
2017-09-26 19:19:25 +0800

    Add comment on check-salt-minion script

--- check-salt-minion.sh
+++ check-salt-minion.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e -x
 
+# Wait until salt-minion docker image is downloaded
 for i in $(seq 20); do
   if [ -x $(docker images -q mhdali/salt-minion:jessie) ]; then
     sleep 5
@@ -9,6 +10,7 @@ for i in $(seq 20); do
   fi
 done
 
+# Delete salt-minion ID and PKI keys
 if [ -f /opt/salt-minion/minion_id ]; then
   sudo rm -fr /opt/salt-minion/{minion_id,pki}
 fi
