8caa424403bc3a2405f770f558d7f1ebc68c97c0
Unknown <chriswayg@gmail.com>
2018-05-25 18:00:09 +0800

    Source variables for each version to build

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-vm_version='1.3.0'
-iso_md5_checksum='1ee0a4abb95e1684ca1aa7c66218398d'
-vm_description='RancherOS with Alpine console (without VirtualBox Guest Additions). [Packer GitHub Source](https://github.com/chriswayg/rancheros-box/tree/basic)'
-vagrantcloud_token='pVOi8vcIvCZDkQ.atlasv1.oeowDxv73MPyW74jBQTjzZ38DeU3SI2k57PDxGEzRoLvmzzeFkt2fv4QWgomiZdPZu0'
+# usage ./build.sh version , for example: ./build.sh 1.3.0
+set -o errexit
+set -o nounset
+
+source "RancherOS-common"
+source "RancherOS-${1}"
+
 packer build \
     -var "vm_version=${vm_version}" \
     -var "iso_md5_checksum=${iso_md5_checksum}" \
-    -var "vm_description=${vm_description}" \
+    -var "vm_description=RancherOS ${vm_version} ${vm_description}" \
+    -var "vagrantcloud_box_name=${vagrantcloud_box_name}" \
     -var "vagrantcloud_token=${vagrantcloud_token}" \
     "packer_rancheros.json"
